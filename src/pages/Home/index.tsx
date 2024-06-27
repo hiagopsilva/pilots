5e227d90668ba46636b831b4536bf0c2a54fa8bc
Hiago Prates <hiagohps360@gmail.com>
2024-06-27 12:31:28 -0300

    feat: config navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 import {Container, Text} from './styles'
+import {TouchableOpacity} from 'react-native'
 
-const Home: React.FC = () => {
+type Props = {
+  navigation: any
+}
+
+const Home: React.FC<Props> = ({navigation}) => {
   return (
     <Container>
       <Text>Hello Pilots</Text>
+
+      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
+        <Text>Go to Details</Text>
+      </TouchableOpacity>
     </Container>
   )
 }
