7c77082c34c4fb8722d6af46f2da9a81a0842249
Hiago Prates <hiagohps360@gmail.com>
2024-06-29 20:30:21 -0300

    feat: page details

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,12 +1,91 @@
 import React from 'react'
-import {Container, Text} from './styles'
+import {
+  BirthPilot,
+  Container,
+  ContainerItem,
+  ContainerList,
+  ContentInfo,
+  ContentItem,
+  ContentList,
+  CountryImage,
+  Description,
+  NamePilot,
+  Title,
+  TitleList,
+} from './styles'
 import HeaderDetails from '../../components/HeaderDetails'
 
 const Details: React.FC = () => {
+  const pilotos = [
+    {
+      name: 'Carlo',
+      date: '10 de Julho de 1932',
+      flag: require('../../assets/images/italy.png'),
+    },
+    {
+      name: 'Walter',
+      date: '15 de Dezembro de 1913',
+      flag: require('../../assets/images/italy.png'),
+    },
+    {
+      name: 'Kurt',
+      date: '05 de Novembro de 1921',
+      flag: require('../../assets/images/italy.png'),
+    },
+    {
+      name: 'Fred',
+      date: '21 de Agosto de 1913',
+      flag: require('../../assets/images/italy.png'),
+    },
+    {
+      name: 'René',
+      date: '07 de Abril de 1948',
+      flag: require('../../assets/images/italy.png'),
+    },
+    {
+      name: 'Keith',
+      date: '15 de Julho de 1920',
+      flag: require('../../assets/images/italy.png'),
+    },
+    {
+      name: 'Mario',
+      date: '28 de Fevereiro de 1940',
+      flag: require('../../assets/images/italy.png'),
+    },
+    {
+      name: 'Chris',
+      date: '20 de Junho de 1943',
+      flag: require('../../assets/images/italy.png'),
+    },
+  ]
+
   return (
     <Container>
       <HeaderDetails title="História" />
-      <Text>Details page</Text>
+
+      <ContentInfo>
+        <Title>História da F1</Title>
+        <Description>
+          Conheça os nomes que fizeram história, desde os pioneiros até os
+          campeões inesquecíveis.
+        </Description>
+      </ContentInfo>
+
+      <ContainerList>
+        <TitleList>Pilotos</TitleList>
+
+        <ContentList>
+          {pilotos.map((piloto, index) => (
+            <ContainerItem key={index}>
+              <CountryImage source={piloto.flag} />
+              <ContentItem>
+                <NamePilot>{piloto.name}</NamePilot>
+                <BirthPilot>Nascido em {piloto.date}</BirthPilot>
+              </ContentItem>
+            </ContainerItem>
+          ))}
+        </ContentList>
+      </ContainerList>
     </Container>
   )
 }
--- src/pages/Details/styles.ts
+++ src/pages/Details/styles.ts
@@ -1,15 +1,89 @@
 import {Platform} from 'react-native'
-import {getStatusBarHeight} from 'react-native-iphone-screen-helper'
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from 'react-native-iphone-screen-helper'
 import styled from 'styled-components/native'
+import {moderateScale} from '../../styles'
 
-export const Container = styled.View`
+export const Container = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: Platform.OS === 'ios' ? getBottomSpace() * 4 : 80,
+  },
+})`
   flex: 1;
-  align-items: center;
   padding: 20px;
   padding-top: ${Platform.OS === 'ios' ? getStatusBarHeight() : 20}px;
 `
 
-export const Text = styled.Text`
+export const ContentInfo = styled.View`
+  width: 100%;
+  height: ${moderateScale(118)};
+  padding: 20px 0;
+`
+
+export const Title = styled.Text`
+  font-size: 20px;
   font-family: 'Inter-Bold';
-  font-size: 24px;
+  color: ${(props) => props.theme.colors.neutro900};
+  margin-bottom: ${moderateScale(8)};
+`
+
+export const Description = styled.Text`
+  font-size: 16px;
+  font-family: 'Inter-SemiBold';
+  color: ${(props) => props.theme.colors.neutro500};
+
+  line-height: 24px;
+`
+
+export const ContainerList = styled.View``
+
+export const TitleList = styled.Text`
+  font-size: 12px;
+  font-family: 'Inter-SemiBold';
+  color: ${(props) => props.theme.colors.textInfo};
+  margin-bottom: ${moderateScale(8)};
+`
+
+export const ContentList = styled.View`
+  flex: 1;
+`
+
+export const ContainerItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: ${moderateScale(8)};
+
+  width: 100%;
+  height: ${moderateScale(68)};
+
+  border-radius: ${moderateScale(12)};
+  border: 1px solid ${(props) => props.theme.colors.neutro200};
+  padding: 12px;
+`
+
+export const CountryImage = styled.Image`
+  background-color: red;
+  width: ${moderateScale(28)};
+  height: ${moderateScale(28)};
+  border-radius: 50px;
+`
+
+export const ContentItem = styled.View`
+  margin-left: 16px;
+`
+
+export const NamePilot = styled.Text`
+  font-size: 16px;
+  font-family: 'Inter-Bold';
+  color: ${(props) => props.theme.colors.neutro950};
+  margin-bottom: ${moderateScale(4)};
+`
+
+export const BirthPilot = styled.Text`
+  font-size: 14px;
+  font-family: 'Inter-Medium';
+  color: ${(props) => props.theme.colors.neutro500};
 `
