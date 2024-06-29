c94fc8ec247343411a60a065c792c801043d6b51
Hiago Prates <hiagohps360@gmail.com>
2024-06-29 14:54:28 -0300

    feat: pilots list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,66 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
-import {Container, Text} from './styles'
+import {
+  ChampionIcon,
+  Container,
+  Line,
+  NamePilot,
+  NameTeam,
+  Position,
+  Text,
+  WrapperPilots,
+  WrapperRace,
+} from './styles'
 import {TouchableOpacity} from 'react-native'
 import Header from '../../components/Header'
+import RacerInfo from '../../components/RacerInfo'
 
 type Props = {
   navigation: any
 }
 
 const Home: React.FC<Props> = ({navigation}) => {
+  const pilots = [
+    {
+      position: 1,
+      name: 'Max Verstappen',
+      team: 'Red Bull Racing',
+    },
+    {
+      position: 2,
+      name: 'Charles Leclerc',
+      team: 'Ferrari',
+    },
+    {
+      position: 3,
+      name: 'Lewis Hamilton',
+      team: 'Mercedes',
+    },
+  ]
   return (
     <Container>
       <Header name="Ana Paula" />
+
+      <WrapperRace>
+        <RacerInfo
+          isSecond
+          textRace="Última corrida"
+          textLocalRace="GP de Abu Dhabi"
+          textDateRace="Data Sáb., 2 de Jan., De 2023 12:00"
+          textCircuitLabel="Circuito de Yas Marina Circuit"
+        />
+
+        <WrapperPilots>
+          {pilots.map((pilot) => (
+            <Line key={pilot.position}>
+              <ChampionIcon />
+              <Position>{pilot.position}º</Position>
+              <NamePilot>{pilot.name}</NamePilot>
+              <NameTeam>{pilot.team}</NameTeam>
+            </Line>
+          ))}
+        </WrapperPilots>
+      </WrapperRace>
 
       <Text>Hello Pilots</Text>
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native'
+import {moderateScale} from '../../styles'
 
 export const Container = styled.View`
   flex: 1;
@@ -7,7 +8,62 @@ export const Container = styled.View`
   background-color: ${(props) => props.theme.colors.background};
 `
 
+export const WrapperRace = styled.View`
+  background-color: ${(props) => props.theme.colors.base.white};
+
+  width: 100%;
+  min-height: ${moderateScale(206)};
+  padding: ${moderateScale(20)};
+`
+
 export const Text = styled.Text`
   font-family: 'Inter-Bold';
   font-size: 24px;
+`
+
+export const WrapperPilots = styled.View`
+  width: 100%;
+  height: ${moderateScale(100)};
+
+  flex-direction: column;
+
+  justify-content: space-between;
+
+  border: 1px solid ${(props) => props.theme.colors.neutro300};
+  border-radius: ${moderateScale(12)};
+  padding: 12px;
+`
+
+export const Line = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+
+export const ChampionIcon = styled.View`
+  width: 20px;
+  height: 20px;
+  background-color: red;
+`
+
+export const Position = styled.Text`
+  margin: 0 ${moderateScale(8)};
+
+  font-size: 14px;
+  font-family: 'Inter-SemiBold';
+  color: ${(props) => props.theme.colors.neutro950};
+`
+
+export const NamePilot = styled.Text`
+  font-size: 14px;
+  font-family: 'Inter-SemiBold';
+  color: ${(props) => props.theme.colors.base.black};
+`
+
+export const NameTeam = styled.Text`
+  margin-left: ${moderateScale(8)};
+
+  font-size: 14px;
+  font-family: 'Inter-SemiBold';
+  color: ${(props) => props.theme.colors.neutro400};
+  letter-spacing: -5%;
 `
