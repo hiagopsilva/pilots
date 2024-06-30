8034b04f0a515f1712e542d22542bea0e9ee0ec9
Hiago Prates <hiagohps360@gmail.com>
2024-06-30 15:06:29 -0300

    chore: remove code unused

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 import {Container, ContentHome, WrapperRace} from './styles'
 import Header from '../../components/Header'
